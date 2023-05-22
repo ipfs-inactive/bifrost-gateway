b637efdc2c275e9ecd54576bf88a359ed4c9291b
Will <willscott@gmail.com>
2023-05-22 20:37:11 +0000

    Revert "Enable HTTP1"

--- blockstore_caboose.go
+++ blockstore_caboose.go
@@ -67,9 +67,9 @@ func newCabooseBlockStore(orchestrator, loggingEndpoint string, cdns *cachedDNS)
 		Transport: otelhttp.NewTransport(&customTransport{
 			RoundTripper: &http.Transport{
 				// Increasing concurrency defaults from http.DefaultTransport
-				MaxIdleConns:        200000,
-				MaxConnsPerHost:     20000,
-				MaxIdleConnsPerHost: 20000,
+				MaxIdleConns:        1000,
+				MaxConnsPerHost:     100,
+				MaxIdleConnsPerHost: 100,
 				IdleConnTimeout:     90 * time.Second,
 
 				DialContext: cdns.dialWithCachedDNS,
@@ -86,7 +86,7 @@ func newCabooseBlockStore(orchestrator, loggingEndpoint string, cdns *cachedDNS)
 					InsecureSkipVerify: true,
 					// ServerName:         "strn.pl",
 				},
-				ForceAttemptHTTP2: false,
+				ForceAttemptHTTP2: true,
 			},
 		}),
 	}
